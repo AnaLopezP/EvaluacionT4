fffc6507181f700866b615fa8142e08a6105e852
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-11-19 19:12:07 +0100

    diccionario frecuencias

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -72,3 +72,11 @@ class nodoArbol(object):
             print(raiz.info)
             nodoArbol.postorden(raiz.izq)
 
+
+frecuencias =  {'A': 0.2, 
+                'F': 0.17,
+                '1': 0.13,
+                '3': 0.21,
+                '0': 0.05,
+                'M': 0.09,
+                'T': 0.15}
