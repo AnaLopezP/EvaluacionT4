d76fce5586d876a86b5558dfd6a0be265d2308c2
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-11-19 18:12:05 +0100

    funcion eliminar nodo

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -4,4 +4,22 @@ class nodoArbol(object):
         self.der = None
         self.info = info
     
+    def eliminar_nodo(raiz, clave):
+        x = None
+        if raiz is not None:
+            if clave < raiz.info:
+                raiz.izq, x = nodoArbol.eliminar_nodo(raiz.izq, clave)
+            elif clave > raiz.info:
+                raiz.der, x = nodoArbol.eliminar_nodo(raiz.der, clave)
+            else:
+                x = raiz.info
+                if raiz.izq is None:
+                    raiz = raiz.der
+                elif raiz.der is None:
+                    raiz = raiz.izq
+                else:
+                    raiz.izq, aux = nodoArbol.remplazar(raiz.izq)
+                    raiz.info = aux.info
+        return raiz, x
+
     
